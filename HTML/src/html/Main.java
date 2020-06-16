6beb8e1a20f6b3c629b0ed5f1f9f1d98f0ea383a
IlianaPinto <iliana.pinto04@gmail.com>
2020-06-15 23:41:59 -0600

    quitando imports

--- HTML/src/html/Main.java
+++ HTML/src/html/Main.java
@@ -2,18 +2,11 @@ package html;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 
 public class Main extends javax.swing.JFrame {
 
